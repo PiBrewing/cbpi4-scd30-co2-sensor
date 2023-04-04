eb6dd73cfb6e476fd0d3efa1a0e94ccbf37c969f
avollkopf <43980694+avollkopf@users.noreply.github.com>
2023-04-04 20:42:28 +0200

    fix

--- cbpi4-scd30_CO2_Sensor/__init__.py
+++ cbpi4-scd30_CO2_Sensor/__init__.py
@@ -23,7 +23,7 @@ class SCD30_Config(CBPiExtension):
         self._task = asyncio.create_task(self.init_sensor())
 
     async def init_sensor(self):
-        plugin = await self.cbpi.plugin.load_plugin_list("cbpi4-scd30-CO2-Sensor")
+        plugin = await self.cbpi.plugin.load_plugin_list("cbpi4-scd30_CO2_Sensor")
         self.version=plugin[0].get("Version","0.0.0")
 
         self.scd30_update = self.cbpi.config.get("scd30_update", None)
